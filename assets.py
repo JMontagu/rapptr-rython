b2d3966b82fde4e35f2f66200dd533004c7bfcf5
Jade Montagu <jade@jademontagu.com>
2017-05-15 08:17:37 +0100

    Init with stuff

---
+++ assets.py
@@ -0,0 +1,16 @@
+assets = [
+    {
+        'AssetName': 'Red Jelly Beans',
+        'AssetClass': 'Equity',
+        'Quantity': 100,
+        'CountryOfIssue': 'AUD',
+        'CallOrPut': ''
+    },
+    {
+        'AssetName': 'Cadbury Creme Eggs',
+        'AssetClass': 'Equity',
+        'Quantity': 5,
+        'CountryOfIssue': 'UK',
+        'CallOrPut': 'Call'
+    },
+]
